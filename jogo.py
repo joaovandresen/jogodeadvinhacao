6f3292c2cfb146dff560199bd4dda965a9cdf650
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-07-31 12:17:47 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,10 @@
+import random
+print("🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆")
+print("🏆 Boas vindas ao:       ")
+print("🏆 jogo de advinhação do ")
+print("🏆 João Vandresen      🏆")
+print("🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆🏆")
+
+numeroSecreto= random.randrange(0,100)
+totalTentativas = 0
+pontos = 100
