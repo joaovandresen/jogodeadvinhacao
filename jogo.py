1ef4cd73ce90a6ccd873b0922a98a90348ca543e
joaovandresen <joao.vandresen.costa@escola.pr.gov.br>
2025-08-04 13:54:08 +0000

    aula 3 finalização loop

--- jogo.py
+++ jogo.py
@@ -15,14 +15,28 @@ print("(1) - Fácil (2) - Médio (3) - Difícil")
 
 nivel = int(input("Escolha um nível: "))
 
-if(nivel == 1):
+if (nivel == 1):
     print("Que nível fácil, franguinho")
     totalTentativas = 20
-if(nivel == 2):
+if (nivel == 2):
     print("Você tem medos de desafio, galizé")
     totalTentativas = 10
-if(nivel == 3):
+if (nivel == 3):
     print("Você já é um galizé, boa sorte")
     totalTentativas = 5
 else:
-    print("Esse nível não existe")
+    print("Esse nível não existe")    
+
+for rodada  in range (1, totalTentativas +1):
+    print("Tentativa {} de {}" .format(rodada,totalTentativas) )
+    chute_str = input("Digite um número entre 1 e 100: ")
+    chute = int(chute_str)
+
+    if(chute < 1 or > 100):
+        print("Número invalido")
+        continue
+
+    acertou = chute == numeroSecreto
+    maior = chute > numeroSecreto
+    menor = chute < numeroSecreto
+    
